8f163bf5dd3af669e76a602e7b9b74e2ea883858
Jyeshtha Vartak <jyeshtha.vartak@gmail.com>
2020-05-20 15:52:53 +0530

    landing page update

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -14,10 +14,58 @@ function getCookie(name) {
     return cookieValue;
 }
 
+function getDistance(ele){
+    var scrollTop = $('.messages').offset().top,
+    elementOffset = ele.offset().top,
+    distance      = (elementOffset - scrollTop);
+    return distance;
+}
+
+function getBackground(){
+    var ins = Math.abs(getDistance($('.introduction'))),
+        fea = Math.abs(getDistance($('.features'))),
+        cta = Math.abs(getDistance($('.cta')));
+
+    if (ins < fea && ins < cta){
+        return '1';
+    }
+    else if (fea < ins && fea < cta){
+        return '2';
+    }
+    else if (cta < ins && cta < fea){
+        return '3';
+    }
+}
+
 
 $(document).ready(function(){
 
-    $(".collection-item").click(function(){
+    $(function () {
+        var $win = $(window);
+
+        $win.scroll(function () {
+            bg = getBackground();
+            console.log(bg);
+            if (bg == 1){
+                // $('.introduction').css(
+                //     {'background-attachment' : 'fixed'}
+                // );
+            }
+            else if (bg == 2){
+                // $('.features').css(
+                //     {'background-attachment' : 'fixed'}
+                // );
+            }
+            else if(bg == 3){
+                // $('.cta').css(
+                //     {'background-attachment' : 'fixed'}
+                // );
+            }
+        });
+    });
+
+
+    $('.collection-item').click(function(){
         var status = $(this).attr('task-status');
         var title = $(this).attr('title');
         var tlist = $(this).attr('list');
@@ -63,7 +111,7 @@ $(document).ready(function(){
             },
         });
         }
-      });
+    });
 
     function status() {
         var stat = $(".collection-item");
@@ -76,10 +124,9 @@ $(document).ready(function(){
               $(this).removeClass("checked");
           }
         });
-      }
-      status();
+    }
+    status();
 
-    
 });
 
 
